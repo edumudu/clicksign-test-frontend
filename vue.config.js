ee1e8f1ff49be462063b369b4bc90febee8d3a56
Eduardo Wesley <eduardomudutiu@gmail.com>
2021-09-09 00:52:09 -0300

    chore: add vue-svg-loader

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,33 @@
 module.exports = {
   lintOnSave: false,
+
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .oneOf('inline')
+      .resourceQuery(/inline/)
+      .use('vue-loader')
+      .loader('vue-loader-v16')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: [
+            { prefixIds: true },
+          ],
+        },
+      })
+      .end()
+      .end()
+      .oneOf('external')
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: 'assets/[name].[hash:8].[ext]',
+      });
+  },
 };
