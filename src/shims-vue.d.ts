2d93306d7cc56423bf685a651a17b1b3627c8fc9
Eduardo Wesley <eduardomudutiu@gmail.com>
2021-09-13 00:52:58 -0300

    chore: add typing for vue-svg-loader impoerts

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,3 +4,16 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.svg?inline' {
+  import Vue, { VueConstructor } from 'vue';
+
+  const content: VueConstructor<Vue>;
+  export default content;
+}
+
