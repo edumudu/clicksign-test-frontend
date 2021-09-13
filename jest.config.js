39a23da2660a5f99ea276cc7e1151a96f0208963
Eduardo Wesley <eduardomudutiu@gmail.com>
2021-09-13 00:51:01 -0300

    chore: configure jest to load svg from vue-svg-load correctly

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+
+  moduleNameMapper: {
+    '^@\\/(.+)/(.*\\.svg)\\?inline$': '<rootDir>/src/$1/$2',
+  },
+
   transform: {
     '^.+\\.vue$': 'vue-jest',
+    '^.+\\.svg$': 'vue-jest',
   },
 };
